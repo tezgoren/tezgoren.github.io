54f71967adf0c300c59d747661e5156075349057
ensar0zturk <ensaraliozturk456@gmail.com>
2025-03-24 22:52:00 +0300

    kodlar 24.03 akşam
    kodlar

---
+++ PomoDream/PomoDream/WebForm1.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PomoDream
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {// Ayarlar butonuna tıklandığında paneli açar
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                Session["timeLeft"] = 25 * 60; // Varsayılan süre (25 dakika)
+                UpdateLabel();
+            }
+            // Place page-specific code here.
+        }
+        protected void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlSettings.CssClass = "settings-panel open"; // Paneli aç
+            btnSettings.Visible = false; // Ayarlar butonunu gizle
+            btnMusic.Visible = false;
+            btnPuzzles.Visible = false;
+        }
+
+        // Kapat butonuna tıklandığında paneli kapatır ve ayarlar butonunu tekrar gösterir
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            pnlSettings.CssClass = "settings-panel"; // Paneli kapat
+            btnPuzzles.Visible = true; // Bulmacalar butonunu tekrar göster
+            btnSettings.Visible = true;
+            btnMusic.Visible = true;
+        }
+
+        protected void btnMusic_Click(object sender, EventArgs e)
+        {
+            pnlMusic.CssClass = "settings-panel open"; // Müzikler panelini aç
+            btnSettings.Visible = false; // Ayarlar butonunu gizle
+            btnMusic.Visible = false;
+            btnPuzzles.Visible = false;
+        }
+
+        // Müzikler panelini kapat
+        protected void btnCloseMusic_Click(object sender, EventArgs e)
+        {
+            pnlMusic.CssClass = "settings-panel"; // Müzikler panelini kapat
+            btnPuzzles.Visible = true; // Bulmacalar butonunu tekrar göster
+            btnSettings.Visible = true;
+            btnMusic.Visible = true;
+        }
+
+        protected void btnPuzzles_Click(object sender, EventArgs e)
+        {
+            pnlPuzzles.CssClass = "settings-panel open"; // Bulmacalar panelini aç
+            btnSettings.Visible = false; // Ayarlar butonunu gizle
+            btnMusic.Visible = false;
+            btnPuzzles.Visible = false;
+        }
+
+        protected void btnClosePuzzles_Click(object sender, EventArgs e)
+        {
+            pnlPuzzles.CssClass = "settings-panel"; // Bulmacalar panelini kapat
+            btnPuzzles.Visible = true; // Bulmacalar butonunu tekrar göste
+            btnSettings.Visible=true;
+            btnMusic.Visible=true;
+        }
+        protected void btnSetTime_Click(object sender, EventArgs e)
+        {
+            int minutes;
+            if (int.TryParse(txtTime.Text, out minutes) && minutes > 0)
+            {
+                Session["timeLeft"] = minutes * 60;
+                UpdateLabel();
+            }
+        }
+
+        protected void btnStart_Click(object sender, EventArgs e)
+        {
+            Timer1.Enabled = true;
+        }
+
+        protected void btnStop_Click(object sender, EventArgs e)
+        {
+            Timer1.Enabled = false;
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            Session["timeLeft"] = int.Parse(txtTime.Text) * 60;
+            UpdateLabel();
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            int timeLeft = (int)Session["timeLeft"];
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                Session["timeLeft"] = timeLeft;
+                UpdateLabel();
+            }
+            else
+            {
+                Timer1.Enabled = false;
+            }
+        }
+
+        private void UpdateLabel()
+        {
+            int timeLeft = (int)Session["timeLeft"];
+            int minutes = timeLeft / 60;
+            int seconds = timeLeft % 60;
+            lblTime.Text = $"{minutes:D2}:{seconds:D2}";
+        }
+
+
+    }
+}
---
+++ PomoDream/PomoDream/WebForm1.aspx.designer.cs
@@ -0,0 +1,188 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik olarak oluşturulmuş>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler hatalı davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik olarak oluşturulmuş>
+//------------------------------------------------------------------------------
+
+namespace PomoDream
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// btnSettings denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSettings;
+
+        /// <summary>
+        /// pnlSettings denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlSettings;
+
+        /// <summary>
+        /// btnClose denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClose;
+
+        /// <summary>
+        /// btnMusic denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMusic;
+
+        /// <summary>
+        /// pnlMusic denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlMusic;
+
+        /// <summary>
+        /// btnCloseMusic denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCloseMusic;
+
+        /// <summary>
+        /// btnPuzzles denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPuzzles;
+
+        /// <summary>
+        /// pnlPuzzles denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlPuzzles;
+
+        /// <summary>
+        /// btnClosePuzzles denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClosePuzzles;
+
+        /// <summary>
+        /// ScriptManager1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// txtTime denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTime;
+
+        /// <summary>
+        /// btnSetTime denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSetTime;
+
+        /// <summary>
+        /// UpdatePanel1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
+        /// lblTime denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTime;
+
+        /// <summary>
+        /// btnStart denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnStart;
+
+        /// <summary>
+        /// btnStop denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnStop;
+
+        /// <summary>
+        /// btnReset denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnReset;
+
+        /// <summary>
+        /// Timer1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
+    }
+}
